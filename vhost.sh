1eedef49be3271a0d12f95ddfa746b50a8af56f6
ilya <anufrievilya@gmail.com>
2019-03-04 17:45:46 +0300

    test

--- vhost.sh
+++ vhost.sh
@@ -1,1 +1,30 @@
 #!/bin/bash
+
+VIRTUALHOST=$1
+VERTDIRECTORY=$1
+WEBMASTER_MAIL=$2
+
+
+
+cat >  /root/backup/test.file.txt <<EOF
+test file
+hell
+hell
+test hell
+EOF
+
+{
+echo "stroka" 
+echo "str2"
+} >> file.txt
+
+cat > vhost.conf <<EOF
+<VirtualHost *:80>
+     ServerAdmin  $WEBMASTER_MAIL
+     DocumentRoot "$VERTDIRECTORY/docs/dummy-host.example.com"
+     ServerName "$VERTDIRECTORY.example.com"
+     ServerAlias "www.$VERTDIRECTORY.example.com"
+     ErrorLog "/var/log/httpd/dummy-host.example.com-error_log"
+     CustomLog "/var/log/httpd/dummy-host.example.com-access_log" common
+</VirtualHost>
+EOF
